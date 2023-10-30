064d8b9b2b2d9150339813e87d2f600121f1fa42
chypaka6ra <14cs95@gmail.com>
2023-10-30 12:11:07 +0300

    Update indexes.js

--- widgets/indexes.js
+++ widgets/indexes.js
@@ -5,71 +5,63 @@ const [
     widgetEmptyStateTemplate,
     WidgetWithInstrument,
     widgetDefaultHeaderTemplate,
-    widgetUnsupportedInstrumentTemplate,
+    widgetWithInstrumentBodyTemplate,
     widgetStackSelectorTemplate
   },
   { html,
     css,
     when,
-    attr,
-    Updates },
-  { WIDGET_TYPES, TRADER_DATUM },
-  { normalize, spacing },
-  {bodyFont,
+    ref,
+    repeat,
+    observable,
+    Observable },
+  { WIDGET_TYPES, TRADER_DATUM, TRADER_CAPS},
+  { priceCurrencySymbol,
+    formatQuantity,
+    formatDate,
+    formatPriceWithoutCurrency },
+  {buy,
     fontSizeWidget,
     paletteBlack,
     paletteGrayBase,
     paletteGrayDark1,
-    paletteGrayDark2,
+    paletteGrayDark4,
     paletteGrayLight1,
     paletteGrayLight2,
-    paletteGrayLight3,
-    paletteGreenDark1,
-    paletteGreenLight2,
-    paletteRedDark1,
-    paletteRedLight3,
     paletteWhite,
-    darken,
+    sell,
     themeConditional,
-    toColorComponents}
+    toColorComponents,
+    lighten,
+    lineHeightWidget},
+  {Tmpl},
+  { AsyncFunction },
+  { invalidate, validate, ValidationError }
 ] = await Promise.all([
   import(`${ppp.rootUrl}/elements/widget.js`),
   import(`${ppp.rootUrl}/vendor/fast-element.min.js`),
   import(`${ppp.rootUrl}/lib/const.js`),
   import(`${ppp.rootUrl}/design/styles.js`),
   import(`${ppp.rootUrl}/design/design-tokens.js`),
+  import(`${ppp.rootUrl}/lib/tmpl.js`),
+  import(`${ppp.rootUrl}/vendor/fast-utilities.js`),
+  import(`${ppp.rootUrl}/lib/ppp-errors.js`),
   import(`${ppp.rootUrl}/elements/button.js`),
   import(`${ppp.rootUrl}/elements/query-select.js`),
+  import(`${ppp.rootUrl}/elements/snippet.js`),
   import(`${ppp.rootUrl}/elements/text-field.js`),
   import(`${ppp.rootUrl}/elements/widget-controls.js`)
 ]);
 
 
 
-export const TabletWidgetTemplate = html`
+export const listWidgetTemplate = html`
   <template>
     <div class="widget-root">
       ${widgetDefaultHeaderTemplate()}
       <div class="widget-body">
         ${widgetStackSelectorTemplate()}
-        ${when(
-  (x) => !x.instrument?.symbol,
-  html`${html.partial(
-    widgetEmptyStateTemplate('Выберите инструмент.')
-  )}`
-)}
-        ${widgetUnsupportedInstrumentTemplate()}
-        <div
-          class="table-holder"
-          ?hidden="${(x) =>
-  !x.instrument?.symbol ||
-  (x.instrument && x.instrumentTrader && x.unsupportedInstrument)}"
-        >
-          <div class="table-holder-inner">
-            <div class="toolbar"></div>
-            <div class="table"></div>
-          </div>
-        </div>
+        ${widgetWithInstrumentBodyTemplate(html` <div></div> `)}
         <ppp-widget-notifications-area></ppp-widget-notifications-area>
       </div>
       <ppp-widget-resize-controls></ppp-widget-resize-controls>
@@ -77,125 +69,47 @@ export const TabletWidgetTemplate = html`
   </template>
 `;
 
-export const TableWidgetStyles = css`
+export const listWidgetStyles = css`
   ${normalize()}
   ${widgetStyles()}
-  ${spacing()}
 `;
 
-export class TableWidget extends WidgetWithInstrument {
-
-
-  // Ready to receive realtime updates
-  @attr({ mode: 'boolean' })
-  ready;
-
-
-
-  css(dt) {
-    const value = dt.$value;
-
-    if (typeof value === 'object') return value.createCSS();
-
-    return value;
+export class ListWidget extends WidgetWithInstrument {
+  constructor() {
+    super();
   }
 
   async connectedCallback() {
-    this.ready = false;
-
     super.connectedCallback();
-
-    if (!this.document.tableTrader) {
-      return this.notificationsArea.error({
-        text: 'Отсутствует трейдер котировок.',
-        keep: true
-      });
-    }
-
-    if (!this.document.tradesTrader) {
-      return this.notificationsArea.error({
-        text: 'Отсутствует трейдер ленты сделок.',
-        keep: true
-      });
-    }
   }
 
-
   async disconnectedCallback() {
-    if (this.tableTrader) {
-      await this.tableTrader.unsubscribeFields?.({
-        source: this,
-        fieldDatumPairs: {
-          traderEvent: TRADER_DATUM.TRADER
-        }
-      });
-    }
-
-    if (this.tradesTrader) {
-      await this.tradesTrader.unsubscribeFields?.({
-        source: this,
-        fieldDatumPairs: {
-          print: TRADER_DATUM.MARKET_PRINT
-        }
-      });
-    }
-
     super.disconnectedCallback();
   }
 
-  resizeTable() {
-    Updates.enqueue(() => {
-      if (this.table) {
-        const { width, height } = getComputedStyle(this);
-
-        if (this.stackSelector.hasAttribute('hidden')) {
-          this.table.resize(parseInt(width) - 2, parseInt(height) - 32);
-        } else {
-          this.table.resize(
-            parseInt(width) - 2,
-            parseInt(height) -
-            32 -
-            parseInt(getComputedStyle(this.stackSelector).height)
-          );
-        }
-      }
-    });
-  }
-
-  onResize({ width, height }) {
-    this.resizeTable();
-  }
-
-  restack() {
-    super.restack();
-    this.resizeTable();
-  }
+  async validate() {}
 
   async submit() {
     return {
-      $set: {
-        tradesTraderId: this.container.tradesTraderId.value
-      }
+      $set: {}
     };
   }
 }
 
 export async function widgetDefinition() {
   return {
-    type: WIDGET_TYPES.FRAME,
+    type: WIDGET_TYPES.LIST,
     collection: 'PPP',
-    title: html`Индексы`,
-    description: html`Виджет
-      <span class="positive">Таблица</span> отображает отросшие акции от закрытия.`,
-    customElement: TableWidget.compose({
-      template: TabletWidgetTemplate,
-      styles: TableWidgetStyles
+    title: html`Список`,
+    description: html`<span class="positive">Список</span> позволяет создавать
+      листинги инструментов и любых других данных, которые можно оформить в
+      таблицу.`,
+    customElement: ListWidget.compose({
+      template: listWidgetTemplate,
+      styles: listWidgetStyles
     }).define(),
-    defaultWidth: 600,
+    minWidth: 275,
     minHeight: 120,
-    minWidth: 140,
-    settings: html`
-    `
+    defaultWidth: 620
   };
 }
-
